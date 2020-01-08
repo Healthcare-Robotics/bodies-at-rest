28ad4965ed9f278b1e5b6fd2b32a60bb59e1c0cc
hc laptop <henryclever@gmail.com>
2020-01-07 23:08:03 -0500

    added file to auto download all synth data

---
+++ PressurePose/download_synth.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+mkdir -p ../data_BR/synth/quick_test
+wget -O ../data_BR/synth/quick_test/test_rollpi_f_lay_set23to24_3000_qt_convnet_1_anglesDC_184000ct_128b_x1pm_tnh_clns10p_100e_2e-05lr.p quick_test https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/IAPI0X/ODHP5Q
+wget -O ../data_BR/synth/quick_test/test_rollpi_f_lay_set23to24_3000_qt_convnet_1_anglesDC_184000ct_128b_x5pm_tnh_clns10p_100e_00002lr.p quick_test https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/IAPI0X/7UB15N
+
+mkdir -p ../data_BR/synth/general
+wget -O ../data_BR/synth/general/test_rollpi_f_lay_set23to24_3000.p general https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/IAPI0X/RSZDMT
+wget -O ../data_BR/synth/general/test_rollpi_m_lay_set23to24_3000.p general https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/IAPI0X/BC4SYE
+wget -O ../data_BR/synth/general/test_rollpi_plo_f_lay_set23to24_3000.p general https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/IAPI0X/EGBWFG
+wget -O ../data_BR/synth/general/test_rollpi_plo_m_lay_set23to24_3000.p general https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/IAPI0X/BQY7CM
+wget -O ../data_BR/synth/general/train_rollpi_f_lay_set10to17_16000.p general https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/IAPI0X/OQ6CRY
+wget -O ../data_BR/synth/general/train_rollpi_f_lay_set18to22_10000.p general https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/IAPI0X/XLEGZL
+wget -O ../data_BR/synth/general/train_rollpi_m_lay_set10to17_16000.p general https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/IAPI0X/QYBFYB
+wget -O ../data_BR/synth/general/train_rollpi_m_lay_set18to22_10000.p general https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/IAPI0X/JNN9TN
+wget -O ../data_BR/synth/general/train_rollpi_plo_f_lay_set10to17_16000.p general https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/IAPI0X/XDUWCQ
+wget -O ../data_BR/synth/general/train_rollpi_plo_f_lay_set18to22_10000.p general https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/IAPI0X/U5XT3E
+wget -O ../data_BR/synth/general/train_rollpi_plo_m_lay_set10to17_16000.p general https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/IAPI0X/U8RYPN
+wget -O ../data_BR/synth/general/train_rollpi_plo_m_lay_set18to22_10000.p general https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/IAPI0X/2RFGW1
+
+
+mkdir -p ../data_BR/synth/general_supine
+wget -O ../data_BR/synth/general_supine/test_roll0_f_lay_set14_1500.p general_supine https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/IAPI0X/FI0VNN
+wget -O ../data_BR/synth/general_supine/test_roll0_m_lay_set14_1500.p general_supine https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/IAPI0X/DXJT6X
+wget -O ../data_BR/synth/general_supine/test_roll0_plo_f_lay_set14_1500.p general_supine https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/IAPI0X/NUJDPK
+wget -O ../data_BR/synth/general_supine/test_roll0_plo_m_lay_set14_1500.p general_supine https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/IAPI0X/WCXMPP
+wget -O ../data_BR/synth/general_supine/train_roll0_f_lay_set10to13_8000.p general_supine https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/IAPI0X/YKFPT2
+wget -O ../data_BR/synth/general_supine/train_roll0_f_lay_set5to7_5000.p general_supine https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/IAPI0X/3ZBEOZ
+wget -O ../data_BR/synth/general_supine/train_roll0_m_lay_set10to13_8000.p general_supine https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/IAPI0X/Q7FQQF
+wget -O ../data_BR/synth/general_supine/train_roll0_m_lay_set5to7_5000.p general_supine https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/IAPI0X/FINXOY
+wget -O ../data_BR/synth/general_supine/train_roll0_plo_f_lay_set10to13_8000.p general_supine https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/IAPI0X/1ERM9V
+wget -O ../data_BR/synth/general_supine/train_roll0_plo_f_lay_set5to7_5000.p general_supine https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/IAPI0X/WXH0EE
+wget -O ../data_BR/synth/general_supine/train_roll0_plo_m_lay_set10to13_8000.p general_supine https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/IAPI0X/XAQOR6
+wget -O ../data_BR/synth/general_supine/train_roll0_plo_m_lay_set5to7_5000.p general_supine https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/IAPI0X/EVILSC
+
+
+mkdir -p ../data_BR/synth/crossed_legs
+wget -O ../data_BR/synth/crossed_legs/test_roll0_xl_f_lay_set1both_500.p crossed_legs https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/IAPI0X/H2HSOY
+wget -O ../data_BR/synth/crossed_legs/test_roll0_xl_m_lay_set1both_500.p crossed_legs https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/IAPI0X/UXI6WH
+wget -O ../data_BR/synth/crossed_legs/train_roll0_xl_f_lay_set2both_4000.p crossed_legs https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/IAPI0X/JWJXY7
+wget -O ../data_BR/synth/crossed_legs/train_roll0_xl_m_lay_set2both_4000.p crossed_legs https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/IAPI0X/EE8GBK
+
+
+mkdir -p ../data_BR/synth/hands_behind_head
+wget -O ../data_BR/synth/hands_behind_head/test_roll0_plo_hbh_f_lay_set4_500.p hands_behind_head https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/IAPI0X/AP5UJZ
+wget -O ../data_BR/synth/hands_behind_head/test_roll0_plo_hbh_m_lay_set1_500.p hands_behind_head https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/IAPI0X/YUX2AS
+wget -O ../data_BR/synth/hands_behind_head/train_roll0_plo_hbh_f_lay_set1to2_2000.p hands_behind_head https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/IAPI0X/UY3JH5
+wget -O ../data_BR/synth/hands_behind_head/train_roll0_plo_hbh_m_lay_set2pa1_2000.p hands_behind_head https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/IAPI0X/HNQNYN
+
+
+mkdir -p ../data_BR/synth/prone_hands_up
+wget -O ../data_BR/synth/prone_hands_up/test_roll0_plo_phu_f_lay_set1pa3_500.p prone_hands_up https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/IAPI0X/HXNT72
+wget -O ../data_BR/synth/prone_hands_up/test_roll0_plo_phu_m_lay_set1pa3_500.p prone_hands_up https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/IAPI0X/GQAAVV
+wget -O ../data_BR/synth/prone_hands_up/train_roll0_plo_phu_f_lay_set2pl4_4000.p prone_hands_up https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/IAPI0X/LQNK2O
+wget -O ../data_BR/synth/prone_hands_up/train_roll0_plo_phu_m_lay_set2pl4_4000.p prone_hands_up https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/IAPI0X/NHMXMU
+
+
+
+mkdir -p ../data_BR/synth/straight_limbs
+wget -O ../data_BR/synth/straight_limbs/test_roll0_sl_f_lay_set1both_500.p straight_limbs https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/IAPI0X/WYD4EM
+wget -O ../data_BR/synth/straight_limbs/test_roll0_sl_m_lay_set1both_500.p straight_limbs https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/IAPI0X/QUAOFC
+wget -O ../data_BR/synth/straight_limbs/train_roll0_sl_f_lay_set2pl3pa1_4000.p straight_limbs https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/IAPI0X/CBFQFG
+wget -O ../data_BR/synth/straight_limbs/train_roll0_sl_m_lay_set2pa1_4000.p straight_limbs https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/IAPI0X/DQPAIV
+
+
+
