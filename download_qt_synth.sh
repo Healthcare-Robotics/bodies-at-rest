988a8db188a5d6a59e10e30ce7aaadd5658f32c2
Henry M. Clever <henryclever@gmail.com>
2020-01-07 19:09:05 -0800

    Update download_qt_synth.sh

--- download_qt_synth.sh
+++ download_qt_synth.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-mkdir -p data_BR/synth/quick_test
-wget -O data_BR/synth/quick_test/test_rollpi_f_lay_set23to24_3000_qt_convnet_1_anglesDC_184000ct_128b_x1pm_tnh_clns10p_100e_2e-05lr.p quick_test https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/IAPI0X/ODHP5Q
-wget -O data_BR/synth/quick_test/test_rollpi_f_lay_set23to24_3000_qt_convnet_1_anglesDC_184000ct_128b_x5pm_tnh_clns10p_100e_00002lr.p quick_test https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/IAPI0X/7UB15N
+mkdir -p ../data_BR/synth/quick_test
+wget -O ../data_BR/synth/quick_test/test_rollpi_f_lay_set23to24_3000_qt_convnet_1_anglesDC_184000ct_128b_x1pm_tnh_clns10p_100e_2e-05lr.p quick_test https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/IAPI0X/ODHP5Q
+wget -O ../data_BR/synth/quick_test/test_rollpi_f_lay_set23to24_3000_qt_convnet_1_anglesDC_184000ct_128b_x5pm_tnh_clns10p_100e_00002lr.p quick_test https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/IAPI0X/7UB15N
