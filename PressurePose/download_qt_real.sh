f98d24e1605a0ec30a5513595c57a219696e56c9
hc laptop <henryclever@gmail.com>
2020-04-05 15:00:17 -0400

    minor

--- PressurePose/download_qt_real.sh
+++ PressurePose/download_qt_real.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 mkdir -p ../data_BR/real/S103
 
-wget -O ../data_BR/real/S103/participant_info_red.p S103 https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/KOA4ML/B8B0KJ
-wget -O ../data_BR/real/S103/prescribed.p S103 https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/KOA4ML/5I4DHE
-wget -O ../data_BR/real/S103/p_select.p S103 https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/KOA4ML/T0IWI8
+wget -O ../data_BR/real/S103/participant_info_red.p S103 https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/KOA4ML/HFW8JZ
+wget -O ../data_BR/real/S103/prescribed.p S103 https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/KOA4ML/1X6YXX
+wget -O ../data_BR/real/S103/p_select.p S103 https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/KOA4ML/SABD5Q
