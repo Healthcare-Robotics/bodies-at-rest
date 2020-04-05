290dd952a52d794236ad3885386ce71427f47bd1
hc laptop <henryclever@gmail.com>
2020-04-05 19:26:36 -0400

    added new script to download trained PressureNet models that are the best performing networks from the paper

---
+++ PressurePose/download_model.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+mkdir -p ../data_BR/convnets
+wget -O ../data_BR/convnets/convnet_1_anglesDC_184000ct_128b_x1pm_tnh_clns20p_100e_2e-05lr.pt convnets https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/IOUBGS/UZACIP
+wget -O ../data_BR/convnets/convnet_2_anglesDC_184000ct_128b_x1pm_0.5rtojtdpth_depthestin_angleadj_tnh_clns20p_100e_2e-05lr.pt convnets https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/IOUBGS/COUXUW
