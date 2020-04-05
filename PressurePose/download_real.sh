742bd17e6360a48e6156caa8d734c9ad5e91b7de
hc laptop <henryclever@gmail.com>
2020-04-05 16:13:50 -0400

    added script to download all the real data.

---
+++ PressurePose/download_real.sh
@@ -0,0 +1,100 @@
+#!/bin/bash
+mkdir -p ../data_BR/real/S103
+wget -O ../data_BR/real/S103/participant_info_red.p S103 https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/KOA4ML/HFW8JZ
+wget -O ../data_BR/real/S103/prescribed.p S103 https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/KOA4ML/1X6YXX
+wget -O ../data_BR/real/S103/p_select.p S103 https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/KOA4ML/SABD5Q
+
+mkdir -p ../data_BR/real/S104
+wget -O ../data_BR/real/S104/participant_info_red.p S104 https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/KOA4ML/NQS4FS
+wget -O ../data_BR/real/S104/prescribed.p S104 https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/KOA4ML/ISZ8YM
+wget -O ../data_BR/real/S104/p_select.p S104 https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/KOA4ML/MMKMTW
+
+mkdir -p ../data_BR/real/S107
+wget -O ../data_BR/real/S107/participant_info_red.p S107 https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/KOA4ML/HQJU46
+wget -O ../data_BR/real/S107/prescribed.p S107 https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/KOA4ML/AFZRHB
+wget -O ../data_BR/real/S107/p_select.p S107 https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/KOA4ML/DMEOXV
+
+mkdir -p ../data_BR/real/S114
+wget -O ../data_BR/real/S114/participant_info_red.p S114 https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/KOA4ML/79SCEY
+wget -O ../data_BR/real/S114/prescribed.p S114 https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/KOA4ML/ZULONP
+wget -O ../data_BR/real/S114/p_select.p S114 https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/KOA4ML/ZJEVOE
+
+mkdir -p ../data_BR/real/S118
+wget -O ../data_BR/real/S118/participant_info_red.p S118 https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/KOA4ML/9SHZL5
+wget -O ../data_BR/real/S118/prescribed.p S118 https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/KOA4ML/MHOB7T
+wget -O ../data_BR/real/S118/p_select.p S118 https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/KOA4ML/6RMG7C
+
+mkdir -p ../data_BR/real/S121
+wget -O ../data_BR/real/S121/participant_info_red.p S121 https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/KOA4ML/YGXEHK
+wget -O ../data_BR/real/S121/prescribed.p S121 https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/KOA4ML/VBQDNS
+wget -O ../data_BR/real/S121/p_select.p S121 https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/KOA4ML/NA1HGA
+
+mkdir -p ../data_BR/real/S130
+wget -O ../data_BR/real/S130/participant_info_red.p S130 https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/KOA4ML/DFNSEC
+wget -O ../data_BR/real/S130/prescribed.p S130 https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/KOA4ML/JGHRAW
+wget -O ../data_BR/real/S130/p_select.p S130 https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/KOA4ML/KQQUS8
+
+mkdir -p ../data_BR/real/S134
+wget -O ../data_BR/real/S134/participant_info_red.p S134 https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/KOA4ML/ZRFWZD
+wget -O ../data_BR/real/S134/prescribed.p S134 https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/KOA4ML/EGBFOR
+wget -O ../data_BR/real/S134/p_select.p S134 https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/KOA4ML/RBRJ6O
+
+mkdir -p ../data_BR/real/S140
+wget -O ../data_BR/real/S140/participant_info_red.p S140 https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/KOA4ML/PIY5NB
+wget -O ../data_BR/real/S140/prescribed.p S140 https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/KOA4ML/9570H2
+wget -O ../data_BR/real/S140/p_select.p S140 https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/KOA4ML/NOELDC
+
+mkdir -p ../data_BR/real/S141
+wget -O ../data_BR/real/S141/participant_info_red.p S141 https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/KOA4ML/8EPUVU
+wget -O ../data_BR/real/S141/prescribed.p S141 https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/KOA4ML/Z6JVFZ
+wget -O ../data_BR/real/S141/p_select.p S141 https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/KOA4ML/EQBTKV
+
+mkdir -p ../data_BR/real/S145
+wget -O ../data_BR/real/S145/participant_info_red.p S145 https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/KOA4ML/MHY81K
+wget -O ../data_BR/real/S145/prescribed.p S145 https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/KOA4ML/ZJJWKC
+wget -O ../data_BR/real/S145/p_select.p S145 https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/KOA4ML/CSFJOH
+
+mkdir -p ../data_BR/real/S151
+wget -O ../data_BR/real/S151/participant_info_red.p S151 https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/KOA4ML/BIEGMJ
+wget -O ../data_BR/real/S151/prescribed.p S151 https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/KOA4ML/WWIR58
+wget -O ../data_BR/real/S151/p_select.p S151 https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/KOA4ML/JSWVY8
+
+mkdir -p ../data_BR/real/S163
+wget -O ../data_BR/real/S163/participant_info_red.p S163 https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/KOA4ML/41M8A4
+wget -O ../data_BR/real/S163/prescribed.p S163 https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/KOA4ML/KZDPBA
+wget -O ../data_BR/real/S163/p_select.p S163 https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/KOA4ML/TSJAHZ
+
+mkdir -p ../data_BR/real/S165
+wget -O ../data_BR/real/S165/participant_info_red.p S165 https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/KOA4ML/FTXL05
+wget -O ../data_BR/real/S165/prescribed.p S165 https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/KOA4ML/Y1W6TB
+wget -O ../data_BR/real/S165/p_select.p S165 https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/KOA4ML/66K5V4
+
+mkdir -p ../data_BR/real/S170
+wget -O ../data_BR/real/S170/participant_info_red.p S170 https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/KOA4ML/ZT9XHJ
+wget -O ../data_BR/real/S170/prescribed.p S170 https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/KOA4ML/ZH75SS
+wget -O ../data_BR/real/S170/p_select.p S170 https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/KOA4ML/GFME15
+
+mkdir -p ../data_BR/real/S179
+wget -O ../data_BR/real/S179/participant_info_red.p S179 https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/KOA4ML/QPXVZM
+wget -O ../data_BR/real/S179/prescribed.p S179 https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/KOA4ML/CEQYPP
+wget -O ../data_BR/real/S179/p_select.p S179 https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/KOA4ML/WHWVMG
+
+mkdir -p ../data_BR/real/S184
+wget -O ../data_BR/real/S184/participant_info_red.p S184 https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/KOA4ML/1CTBCQ
+wget -O ../data_BR/real/S184/prescribed.p S184 https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/KOA4ML/WOPOXU
+wget -O ../data_BR/real/S184/p_select.p S184 https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/KOA4ML/8UXFMU
+
+mkdir -p ../data_BR/real/S187
+wget -O ../data_BR/real/S187/participant_info_red.p S187 https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/KOA4ML/YP9GFU
+wget -O ../data_BR/real/S187/prescribed.p S187 https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/KOA4ML/UMW2MI
+wget -O ../data_BR/real/S187/p_select.p S187 https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/KOA4ML/AYW2UD
+
+mkdir -p ../data_BR/real/S188
+wget -O ../data_BR/real/S188/participant_info_red.p S188 https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/KOA4ML/QPEPYC
+wget -O ../data_BR/real/S188/prescribed.p S188 https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/KOA4ML/7EPFLF
+wget -O ../data_BR/real/S188/p_select.p S188 https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/KOA4ML/7YXOXB
+
+mkdir -p ../data_BR/real/S196
+wget -O ../data_BR/real/S196/participant_info_red.p S196 https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/KOA4ML/3BXXMW
+wget -O ../data_BR/real/S196/prescribed.p S196 https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/KOA4ML/XON1LX
+wget -O ../data_BR/real/S196/p_select.p S196 https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/KOA4ML/43LKDZ
